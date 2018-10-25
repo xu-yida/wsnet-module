210b97362eae4d241dd66c4357d25b8dfc37be8f
Adam <xuyida@ict.acn.cn>
2018-10-25 14:17:55 +0800

    modify result format

--- user_models/sic_priority_routing.c
+++ user_models/sic_priority_routing.c
@@ -159,10 +159,13 @@ int unsetnode(call_t *c) {
 	//PRINT_RESULT("maximal priority0 delay is %"PRId64", maximal priority1 delay is %"PRId64"\n", g_delay_max0, g_delay_max1);
 	//PRINT_RESULT("average delay of priority0 packets is %f, average delay of priority1 packets is %f\n", g_delay_priority0/g_num_r_priority0, g_delay_priority1/g_num_r_priority1);
 	//PRINT_RESULT("node %d consumes energy: %f\n", c->node, battery_consumed(c));
-	PRINT_RESULT("%d , %d, %d, ", g_num_r, g_num_r_priority0, g_num_r_priority1);
-	PRINT_RESULT("%f, %f, %f, ", g_delay/g_num_r, g_delay_priority0/g_num_r_priority0, g_delay_priority1/g_num_r_priority1);
-	PRINT_RESULT("%"PRId64", %"PRId64", ", g_delay_max0, g_delay_max1);
-	PRINT_RESULT("%f, ", battery_consumed(c));
+	if(1 == c->node)
+	{
+		PRINT_RESULT("%d , %d, %d, ", g_num_r, g_num_r_priority0, g_num_r_priority1);
+		PRINT_RESULT("%f, %f, %f, ", g_delay/g_num_r, g_delay_priority0/g_num_r_priority0, g_delay_priority1/g_num_r_priority1);
+		PRINT_RESULT("%"PRId64", %"PRId64", ", g_delay_max0, g_delay_max1);
+		PRINT_RESULT("%f, ", battery_consumed(c));
+	}
 #endif// ADAM_TEST
     while ((neighbor = (struct neighbor *) das_pop(nodedata->neighbors_low)) != NULL) {
         free(neighbor);
